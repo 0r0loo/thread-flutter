afceb912cddc0901dd6947dbc91fc978a0528d89
chotaegyu <chotg5592@gmail.com>
2022-07-17 13:51:04 +0900

    Add: 컬러 추가

---
+++ lib/const/colors.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+// Color
+const color00 = Color(0xFFFFFFFF);
+const color01 = Color(0xFFF6F6F7);
+const color02 = Color(0xFFE6E6E8);
+const color03 = Color(0xFFCDCDD0);
+const color04 = Color(0xFFA0A0A5);
+const color05 = Color(0xFF616166);
+const color06 = Color(0xFF525256);
+const color07 = Color(0xFF38383B);
+const color08 = Color(0xFF2E2E31);
+const color09 = Color(0xFF262629);
+const color09Plus = Color(0xFF000000);
+
+// ETC
+const point = Color(0xFFF9851B);
+const red = Color(0xFFEB5757);
+
+// 객체로 바꾸고싶은데...
+
+// Day Mode
+const dayUi01 = color08;
+const dayUi02 = color04;
+const dayUi03 = color02;
+const dayUi04 = color00;
+
+const dayLine01 = color02;
+const dayLine02 = color04;
+
+const dayBg01 = color00;
+const dayBg02 = color01;
+
+// Sunset Mode
+const sunsetUi01 = Color(0xFF31302B);
+const sunsetUi02 = Color(0xFFA5A39B);
+const sunsetUi03 = Color(0xFFD1CFC9);
+const sunsetUi04 = color00;
+
+const sunsetLine01 = Color(0xFFE8E7E1);
+const sunsetLine02 = Color(0xFFA5A39B);
+
+const sunsetBg01 = color00;
+const sunsetBg02 = Color(0xFFF7F6F2);
+
+// Night Mode
+const nightUi01 = color01;
+const nightUi02 = color04;
+const nightUi03 = color06;
+const nightUi04 = color09;
+
+const nightLine01 = color09;
+const nightLine02 = color09Plus;
+
+const nightBg01 = color07;
+const nightBg02 = color08;
